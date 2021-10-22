6546d595041beb0ad490a507633c80d17da52a67
Łukasz Kot <lukaszkot@ukaszs-MacBook-Pro.local>
2021-10-22 21:50:57 +0200

    Added git ignore.

---
+++ Program.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public char WinPerson { get; set; }
+
+    public bool IsWin { get; set; }
+
+    private bool isX;
+
+    public bool isY
+    {
+        get { return isX; }
+        set { isX = value; }
+    }
+
+    public char[,] Boxes { get; set; } = new char[3, 3];
+
+    public void WriteBoard(int size)
+    {
+        var table = new List<string>();
+        for (var i = 0; i < size; i++)
+        {
+            var row = new List<string>();
+            for (var j = 0; j < size; j++)
+            {
+                row.Add($" {Boxes[i, j]} ");
+            }
+            table.Add(string.Join('|', row));
+        }
+        var tableWith = table[0].Length - 2;
+        var border = string.Join("", Enumerable.Repeat("-", tableWith));
+        var tableAsString = string.Join($"\n {border} \n", table);
+        Console.WriteLine(tableAsString);
+    }
+
+
+
+    private bool IsPlayerWinnerInAnyRow(char player, int size)
+    {
+        for (var i = 0; i < size; i++)
+        {
+            if (IsWinnerInRow(player, i, size))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsWinnerInRow(char player, int rowIndex, int numberOfColumns)
+    {
+        for (var i = 0; i < numberOfColumns; i++)
+        {
+            if (Boxes[rowIndex, i] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsPlayerWinnerInAnyColumn(char player, int size)
+    {
+        for (var i = 0; i < size; i++)
+        {
+            if (IsWinnerInColumn(player, i, size))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsWinnerInColumn(char player, int columnIndex, int numberOfRows)
+    {
+        for (var i = 0; i < numberOfRows; i++)
+        {
+            if (Boxes[i, columnIndex] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsPlayerWinnerInRightCrossing(char player, int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (Boxes[i, i] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsPlayerWinnerInLeftCrossing(char player, int size)
+    {
+        for (var i = 0; i < size; i++)
+        {
+            if (Boxes[i, size - 1 - i] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsPlayerAWinner(char player, int size)
+    {
+        return IsPlayerWinnerInAnyColumn(player, size)
+            || IsPlayerWinnerInAnyRow(player, size)
+            || IsPlayerWinnerInLeftCrossing(player, size)
+            || IsPlayerWinnerInRightCrossing(player, size);
+    }
+
+    public void CheckWin()
+    {
+        CheckWinForPlayer('X', 3);
+        CheckWinForPlayer('Y', 3);
+    }
+
+    private void CheckWinForPlayer(char player, int size)
+    {
+        if (IsPlayerAWinner(player, size))
+        {
+            MarkPlayerAsAWinner(player);
+        }
+    }
+
+    private void MarkPlayerAsAWinner(char player)
+    {
+        IsWin = true;
+        WinPerson = player;
+    }
+
+    public void NotVacantError()
+    {
+        HasError = true;
+        Console.WriteLine();
+        Console.WriteLine("Error: box not vacant!");
+        Console.WriteLine("Press any key to try again..");
+        Console.ReadKey();
+        return;
+    }
+
+    public void DisplayLoss()
+    {
+        Console.WriteLine();
+        Console.WriteLine("No one won.");
+        Console.ReadKey();
+        Environment.Exit(1);
+    }
+
+    public bool HasError;
+
+    public void InitializeData(int size)
+    {
+        for (var i = 0; i < size; i++)
+        {
+            for (var j = 0; j < size; j++)
+            {
+                Boxes[i, j] = ' ';
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int moveCount = 0; // check loss
+        char askMove; // display X or Y in question
+        int selTemp;
+        Program prog = new Program();
+        prog.HasError = false;
+        prog.InitializeData(3);
+        prog.isY = true;
+        Console.WriteLine(" -- Tic Tac Toe -- ");
+        Console.Clear();
+        while (!prog.IsWin)
+        {
+            if (moveCount == 9)
+            {
+                prog.DisplayLoss();
+            }
+            if ((prog.isY) == true) // if is X
+            {
+                askMove = 'X';
+            }
+            else
+            {
+                askMove = 'Y';
+            }
+            Console.Clear();
+            prog.WriteBoard(3);
+            Console.WriteLine();
+            Console.WriteLine("What box do you want to place {0} in? (1-9)", askMove);
+            Console.Write("> ");
+            selTemp = int.Parse(Console.ReadLine());
+            switch (selTemp)
+            {
+                case 1:
+                    if (prog.Boxes[0, 0] == ' ')
+                    {
+                        prog.Boxes[0, 0] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 2:
+                    if (prog.Boxes[0, 1] == ' ')
+                    {
+                        prog.Boxes[0, 1] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 3:
+                    if (prog.Boxes[0, 2] == ' ')
+                    {
+                        prog.Boxes[0, 2] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 4:
+                    if (prog.Boxes[1, 0] == ' ')
+                    {
+                        prog.Boxes[1, 0] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 5:
+                    if (prog.Boxes[1, 1] == ' ')
+                    {
+                        prog.Boxes[1, 1] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 6:
+                    if (prog.Boxes[1, 2] == ' ')
+                    {
+                        prog.Boxes[1, 2] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 7:
+                    if (prog.Boxes[2, 0] == ' ')
+                    {
+                        prog.Boxes[2, 0] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 8:
+                    if (prog.Boxes[2, 1] == ' ')
+                    {
+                        prog.Boxes[2, 1] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                case 9:
+                    if (prog.Boxes[2, 2] == ' ')
+                    {
+                        prog.Boxes[2, 2] = askMove;
+                        moveCount++;
+                    }
+                    else
+                    {
+                        prog.NotVacantError();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong selection entered!");
+                    Console.WriteLine("Press any key to try again..");
+                    Console.ReadKey();
+                    prog.HasError = true;
+                    break;
+            }
+            if (prog.HasError)
+            {
+                prog.CheckWin(); // if error, just check win
+                prog.HasError = !prog.HasError;
+            }
+            else
+            {
+                prog.isY = !prog.isY; // flip boolean
+                prog.CheckWin();
+            }
+        }
+        Console.Clear();
+        prog.WriteBoard(3);
+        Console.WriteLine();
+        Console.WriteLine("The winner is {0}!", prog.WinPerson);
+        Console.ReadKey();
+    }
+}
